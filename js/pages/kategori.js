133b58cb486b9ec42350ec5afc1357717980e37f
Ari Suseta <itsukaakatsuki@gmail.com>
2025-04-22 13:51:11 +0700

    perbaikan kode agar modular

--- js/pages/kategori.js
+++ js/pages/kategori.js
@@ -1,95 +1,106 @@
-import { escapeHTML } from "../utils.js";
+import {createElement, elFollowMe, escapeHTML, getLocalstorage, saveLocalStorage} from "../utils.js";
+
+export function renderKategori(content) {
+	fetch("pages/kategori.html")
+	.then(res => res.text())
+	.then(html => {
+		content.innerHTML = html;
+		setupKategoriPage();
+	});
+}
+
 
 const kategoriKey = "data-kategori";
-export function renderKategori(content) {
-  content.innerHTML = `
-		<div class="mtxl">
-			<section class="kategori-page">
-				<h2>Manajemen Kategori</h2>
-				<form id="form-kategori">
-					<input type="hidden" name="id" />
-					<input type="text" name="nama" placeholder="Nama kategori" required />
-					<button type="submit">Simpan</button>
-				</form>
-	
-				<table class="tabel-kategori">
-					<thead>
-						<tr>
-							<th>No</th>
-							<th>Nama Kategori</th>
-							<th>Aksi</th>
-						</tr>
-					</thead>
-					<tbody id="daftar-kategori"></tbody>
-				</table>
-			</section>
-		</div>
-  `;
+const dataKategori = getLocalstorage(kategoriKey) || [];
 
-  const form = document.querySelector("#form-kategori");
-  const tBody = document.querySelector("#daftar-kategori");
+function addData(form, event){
+	event.preventDefault();
+	const dataInput = {
+		id: Date.now(),
+		nama: form.nama.value
+	}
+	// jika ada data yang sama timpa
+	const index = dataKategori.findIndex(item => item.nama === dataInput.nama);
+	if (index !== -1){
+		dataKategori[index] = dataInput;
+		tampilkanDaftarKategori();
+		form.reset();
+		return;
+	}
+	dataKategori.push(dataInput);
+	saveLocalStorage(kategoriKey, JSON.stringify(dataKategori));
+	tampilkanDaftarKategori();
+	form.reset();
+}
 
-  // 	render tabel
-  function tampilkanKategori() {
-    const data = JSON.parse(localStorage.getItem(kategoriKey) || "[]");
-    tBody.innerHTML = "";
+function setupKategoriPage(){
+	const formEl = document.getElementById("form-kategori");
 
-    data.forEach((item, i) => {
-      const tr = document.createElement("tr");
-      tr.innerHTML = `
-				<td>${i + 1}</td>
-        <td>${item.nama}</td>
-        <td class="text-center">
-          <button data-id="${item.id}" class="edit-btn">Edit</button>
-          <button data-id="${item.id}" class="hapus-btn">Hapus</button>
-        </td>
-			`;
-      tBody.appendChild(tr);
-    });
-  }
+	if (dataKategori!== 0){
+		tampilkanDaftarKategori();
+		formEl.addEventListener("submit", function (e) {
+			addData(this, e);
+		});
+	}
+}
 
-  tampilkanKategori();
-  // 	submit form
-  form.onsubmit = function (e) {
-    e.preventDefault();
-    const id = form.id.value;
-    const nama = escapeHTML(form.nama.value.trim());
-    if (!nama) return;
+function hapusData(id, conf = confirm("Yakin ingin menghapus data ini!")){
+	const index = dataKategori.findIndex(data => data.id === id);
 
-    let data = JSON.parse(localStorage.getItem(kategoriKey) || "[]");
-    if (id) {
-      const index = data.findIndex((k) => k.id.toString() === id);
-      data[index].nama = nama;
-    } else {
-      const newKategori = { id: Date.now(), nama };
-      data.push(newKategori);
-    }
-    localStorage.setItem("data-kategori", JSON.stringify(data));
-    form.id.removeAttribute("value");
-    form.reset();
-    tampilkanKategori();
-  };
+	if (!conf) return;
 
-  // Tombol Edit & Hapus
-  tBody.onclick = function (e) {
-    if (e.target.classList.contains("edit-btn")) {
-      const id = e.target.dataset.id;
-      const data = JSON.parse(localStorage.getItem("data-kategori") || "[]");
-      const kategori = data.find((k) => k.id == id);
-      if (kategori) {
-        form.id.value = kategori.id;
-        form.nama.value = kategori.nama;
-      }
-    }
+	if (index !== -1){
+		dataKategori.splice(index, 1);
+		saveLocalStorage(kategoriKey, JSON.stringify(dataKategori));
+		tampilkanDaftarKategori();
+	}
+}
 
-    if (e.target.classList.contains("hapus-btn")) {
-      const id = e.target.dataset.id;
-      if (confirm("Yakin ingin menghapus kategori ini?")) {
-        let data = JSON.parse(localStorage.getItem("data-kategori") || "[]");
-        data = data.filter((k) => k.id != id);
-        localStorage.setItem("data-kategori", JSON.stringify(data));
-        tampilkanKategori();
-      }
-    }
-  };
+function editData(id) {
+	if (window.innerWidth <= 375) document.getElementById("app-content").scrollIntoView({behavior:"smooth", block: "start"});
+// 	lanjut nanti
+}
+
+function tampilkanDaftarKategori(){
+	const tableBody = document.getElementById("daftar-kategori");
+	tableBody.innerHTML = "";
+	if (tableBody){
+		dataKategori.forEach((data, index) => {
+			const row = createElement({tag:'tr'});
+			const noCell = createElement({tag:"td", textContent: index + 1});
+			const namaCell = createElement({tag:"td", textContent: data.nama});
+			const aksiCell = createElement({
+				tag: "td",
+				attributes: {
+					'style' : "text-align:center"
+				},
+				children:[
+					createElement({
+						tag: "button",
+						attributes: {
+							'type': "button"
+						},
+						textContent: "edit",
+						eventListeners: {
+							'click': () => editData(data.id)
+						}
+					}),
+					createElement({
+						tag: "button",
+						attributes: {
+							'type': "button"
+						},
+						textContent: "hapus",
+						eventListeners: {
+							'click': () => hapusData(data.id)
+						}
+					})
+				]
+			});
+			row.appendChild(noCell);
+			row.appendChild(namaCell);
+			row.appendChild(aksiCell);
+			tableBody.appendChild(row);
+		});
+	}
 }
---
+++ js/router.js
@@ -0,0 +1,31 @@
+import {renderDashboard} from "./pages/dashboard.js";
+import {renderTransaksi} from "./pages/transaksi.js";
+import {renderKategori} from "./pages/kategori.js";
+import {renderRiwayat} from "./pages/riwayat.js";
+import {renderLaporan} from "./pages/laporan.js";
+import {logoutUser} from "./utils.js";
+// load halaman berdasarkan nama halaman
+export function loadPage (page, content){
+	switch (page){
+		case "dashboard":
+			renderDashboard(content);
+			break;
+		case "transaksi":
+			renderTransaksi(content);
+			break;
+		case "kategori":
+			renderKategori(content);
+			break;
+		case "riwayat":
+			renderRiwayat(content);
+			break;
+		case "laporan":
+			renderLaporan(content);
+			break;
+		case "logout":
+			logoutUser();
+			break;
+		default:
+			renderDashboard(content);
+	}
+};
